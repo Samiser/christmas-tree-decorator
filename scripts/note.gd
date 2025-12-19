1f91f19e0aa710ccccfa7037170791515193d17f
Samiser <github@me.samiser.xyz>
2025-12-19 19:06:49 +0000

    major scale note map

--- scripts/note.gd
+++ scripts/note.gd
@@ -6,9 +6,20 @@ enum Timbre { BELL, LIGHT }
 var pitch: int
 var timbre: Timbre
 
+var major_scale_map: Dictionary[int, int] = {
+	0: 0,
+	1: 2,
+	2: 4,
+	3: 5,
+	4: 7,
+	5: 9,
+	6: 11,
+	7: 12,
+}
+
 func _init(p: int, t: Timbre):
 	pitch = p
 	timbre = t
 
 func get_scale() -> float:
-	return pow(2.0, pitch / 12.0)
+	return pow(2.0, major_scale_map[pitch] / 12.0)
