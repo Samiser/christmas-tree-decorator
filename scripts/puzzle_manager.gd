5dd0c228946375727672eadf6fd42a55c3832480
Samiser <github@me.samiser.xyz>
2025-12-19 21:15:52 +0000

    fixed constraints bug

--- scripts/puzzle_manager.gd
+++ scripts/puzzle_manager.gd
@@ -9,31 +9,38 @@ func _init() -> void:
 func _create_puzzles():
 	puzzles.append(_create_puzzle_1())
 	puzzles.append(_create_puzzle_2())
+	puzzles.append(_create_puzzle_3())
 
 func _create_puzzle_1() -> Puzzle:
 	var puzzle = Puzzle.new()
-	puzzle.constraints = [
+	var constraints = [
 		Constraint.PitchAtIndex.new(0, 0),
 		Constraint.PitchAtIndex.new(1, 4),
 	]
+	for constraint in constraints:
+		puzzle.constraints.append(constraint)
 	return puzzle
 
 func _create_puzzle_2() -> Puzzle:
 	var puzzle = Puzzle.new()
-	puzzle.constraints = [
+	var constraints = [
 		Constraint.PitchAtIndex.new(0, 7),
 		Constraint.SamePitch.new(1, 2),
 	]
+	for constraint in constraints:
+		puzzle.constraints.append(constraint)
 	return puzzle
 
 func _create_puzzle_3() -> Puzzle:
 	var puzzle = Puzzle.new()
-	puzzle.constraints = [
+	var constraints = [
 		Constraint.PitchAtIndex.new(0, 0),
 		Constraint.PitchAtIndex.new(3, 7),
 		Constraint.DifferentPitch.new(1, 2),
 		Constraint.DifferentPitch.new(0, 1),
 	]
+	for constraint in constraints:
+		puzzle.constraints.append(constraint)
 	return puzzle
 
 func get_puzzle(index: int) -> Puzzle:
